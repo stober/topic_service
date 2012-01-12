fbe6e32f00c76e3a307d65370674a8b34abe40bf
Jeremy Stober <stober@gmail.com>
2012-01-12 15:30:47 -0600

    Comment cleanup.

--- src/topic_server.py
+++ src/topic_server.py
@@ -7,8 +7,6 @@ import rospy
 import threading
 import copy
 
-# TODO: add a clock argument to service (to get topic data that is current?)
-
 class TopicServer:
 
     def __init__(self, node_name, topic_name, server_name, topic_type, server_type, transform):
@@ -19,19 +17,11 @@ class TopicServer:
         self.cv = threading.Condition(self.lock)
         rospy.Subscriber(topic_name, topic_type, self.topicCallback)
 
+        # There doesn't seem to be a way to construct server_type from topic type dynamically. Sigh.
         self.service = rospy.Service(server_name, server_type, self.returnTopic)
         self.transform = transform
 
     def topicCallback(self, msg):
-        """
-        My guess is that in Python there is really no need for
-        condition variables and a deep copy from topic listener thread
-        local data into self.data. However, this seems like the safest
-        thread safe way to examine the topic in the midst of a single
-        server call without any real knowledge of how or in what
-        context the topic callback is actually called.
-        """
-
         self.cv.acquire()
 
         if self.acquired is False:
@@ -43,6 +33,9 @@ class TopicServer:
 
     def spin(self):
         rospy.spin()
+
+    # Override if you want to make this more complex.
+    # E.g. Wait a certain amount of clock time.
 
     def returnTopic(self, req):
         self.cv.acquire()
